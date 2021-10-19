5072ddc5c300bb1f4f3a5d78886f4cca757bfd4d
Darin Mao <maodarin@gmail.com>
2021-10-19 13:13:47 -0400

    feat: add some test images

--- jigsolve/__main__.py
+++ jigsolve/__main__.py
@@ -1,1 +1,12 @@
-print('pepega')
+import cv2
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Run with a test image')
+    parser.add_argument('path')
+    args = parser.parse_args()
+
+    img = cv2.imread(args.path)
+    cv2.imshow('test', img)
+
+if __name__ == '__main__': main()
