423a82d079bbdcd49d699050334f5c4501a1a377
Bryant Park <52787152+bryantpark04@users.noreply.github.com>
2021-10-20 11:41:29 -0400

    Add docstring to camera.py and add .idea/ to the .gitignore

--- jigsolve/vision/camera.py
+++ jigsolve/vision/camera.py
@@ -1,10 +1,18 @@
 import cv2
 
+
 def capture_image():
+    '''Captures a single frame from the webcam.
+
+    This function creates a VideoCapture from the default webcam and returns a captured frame.
+
+    Returns
+    -------
+    frame : np.ndarray
+        An image from the default webcam.
+    '''
+
     cam = cv2.VideoCapture(0)
     ret, frame = cam.read()
-    cam.release()
+    cam.release() # how to avoid continously initializing and releasing the camera?
     return frame
-
-cv2.imshow('frame', capture_image())
-cv2.waitKey(0)
