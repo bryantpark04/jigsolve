0ae24fb666ac62b23c08b09e5f56862dcb1f668d
Darin Mao <maodarin@gmail.com>
2022-01-27 15:42:15 -0500

    fix: use tuple type in piece model
    old code did not work with python3.8

--- jigsolve/models.py
+++ jigsolve/models.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass, field
 import numpy as np
+from typing import Tuple
 
 @dataclass(eq=False, order=False, frozen=True)
 class PuzzlePiece:
     img: np.ndarray = field(repr=False)
     mask: np.ndarray = field(repr=False)
-    hist: tuple[np.ndarray] = field(repr=False)
+    hist: Tuple[np.ndarray] = field(repr=False)
     img_rot: float
-    box: tuple[int]
-    edges: tuple[int]
+    box: Tuple[int]
+    edges: Tuple[int]
